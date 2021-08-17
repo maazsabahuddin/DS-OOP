ed5540cbe50b7d505f60ef8bf7057763bcea67af
Maaz Techwards <maazsabahuddin@gmail.com>
2021-08-17 11:04:41 +0500

    update code reverse integer

--- Programs/reverse_integer.py
+++ Programs/reverse_integer.py
@@ -1,6 +1,26 @@
 
 
+def absolute_method(integer):
+    count = 0
+    if integer > 0:
+        return integer
+
+    for i in range(0, integer, -1):
+        count += 1
+
+    return count
+
+
+def abs_method(integer):
+    return -integer if integer < 0 else integer
+
+
 def reverse_integer(num):
+
+    flag = False
+    if num < 0:
+        flag = True
+        num = abs_method(num)
 
     rev = 0
     while num > 0:
@@ -8,6 +28,8 @@ def reverse_integer(num):
         rev = (rev * 10) + left
         num = num//10
 
+    if flag:
+        return -rev
     return rev
 
 
@@ -24,5 +46,5 @@ def reverse_integer_by_string(num):
     return ''.join(reverse_string_list)
 
 
-print(reverse_integer(102))
+print(reverse_integer(-102))
 print(reverse_integer_by_string(102))
